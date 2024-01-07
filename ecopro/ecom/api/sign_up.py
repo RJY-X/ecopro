b48f1e74423698714fe6f140e502d1bc74a4687f
Reda Ben <redabendahou0@gmail.com>
2024-01-07 19:31:38 +0100

    login progress

--- ecopro/ecom/api/sign_up.py
+++ ecopro/ecom/api/sign_up.py
@@ -3,29 +3,6 @@ from ecom.forms import SignupForm
 from django.utils.crypto import get_random_string
 from django.contrib.auth.models import User
 
-# def signup(request):
-#     if request.method=='POST':
-#         username = request.POST.get('first-name')
-#         password = request.POST.get('password')
-#         firstname = request.POST.get('first-name')
-#         lastname = request.POST.get('last-name')
-#         email = request.POST.get('email')
-#         form_data = {
-#             'username': username,
-#             'password': password,
-#             'first_name': firstname,
-#             'last_name': lastname,
-#             'email': email,
-#         }
-#         form = SignupForm(data=form_data)
-#         if form.is_valid():
-#             form.save()
-#             return render(request, 'ecom/sign_up.html', {'message': "Signed up successfully"})
-#         else:
-#             return render(request, 'ecom/sign_up.html', {'message': "Form is not valid. Please check your inputs."})
-
-#     else:
-#         return render(request, 'ecom/sign_up.html', {'message':"not sign in"})
 
 def signup(request):
     if request.method == "POST":
@@ -50,10 +27,6 @@ def signup(request):
             return redirect("/")
         else:
             errors = {}
-
-            # for field, err in form.errors.items():
-            #     print(f"Field: {field}, Errors: {', '.join(errors)}")
-            #     errors[field] = err
             for field, err in form.errors.items():
                 print(f"Field: {field}, Errors: {', '.join(err)}")
                 print(', '.join(err))
--- ecopro/ecom/urls.py
+++ ecopro/ecom/urls.py
@@ -7,7 +7,7 @@ app_name = "ecom"
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("login",views.login, name="login"),
+    path("login",views.Alogin, name="login"),
     path("signup",views.signup,name="signup"),
     path("shop",views.shop, name="shop"),
     path("cart",views.cart, name="cart"),
