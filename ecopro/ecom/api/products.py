a83895bcf9530fdd9eccc41e9426c8931810d9bd
egris9 <amjadnaciri73@gmal.com>
2024-01-11 00:16:40 +0100

    my changes bitchezzzzz

--- ecopro/ecom/api/products.py
+++ ecopro/ecom/api/products.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from ecom.api.queries.products.getProductById import get_product_by_id
 
 
 def products(request, id):
     print(f"id - {id}")
-    return render(request, "ecom/products.html")
+    product=get_product_by_id(id)
+    return render(request, "ecom/products.html",{**product})
