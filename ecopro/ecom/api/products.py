5b5b502589a2e7ea37f62d75623f3ff66af8c2d1
othmane bakkass <othmanebakkass@gmail.com>
2024-01-12 22:22:18 +0100

    fix product links inside product page

--- ecopro/ecom/api/products.py
+++ ecopro/ecom/api/products.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from ecom.api.queries.products.getProductById import get_product_by_id
+from ecom.api.queries.products.getPopularProducts import get_popular_products
 
 
 def products(request, id):
-    print(f"id - {id}")
-    product=get_product_by_id(id)
-    return render(request, "ecom/products.html",{**product})
+    product = get_product_by_id(id)
+    similar_products = get_popular_products()
+    return render(
+        request, "ecom/products.html", {**product, "similar_products": similar_products}
+    )
